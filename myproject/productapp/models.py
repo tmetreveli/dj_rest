0aacadef17a135897a05d14c96e5d7f66e16223e
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-04-30 17:30:20 +0400

    added verbose names to models

--- myproject/productapp/models.py
+++ myproject/productapp/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Category Name')
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100, verbose_name='Product Name')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Price')
+    inventory = models.IntegerField(verbose_name='Inventory')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Category')
 
     def __str__(self):
         return self.name
