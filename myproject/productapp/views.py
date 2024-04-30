5abe07f9f80bbd4d4c54970fb55568d4ca4fef8b
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-04-30 18:03:29 +0400

    added views and endpoints

--- myproject/productapp/views.py
+++ myproject/productapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Product, Category
+from .serializers import ProductSerializer, ProductInventorySerializer, CategorySerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'update_inventory':
+            return ProductInventorySerializer
+        return super().get_serializer_class()
+
+    @action(detail=True, methods=['patch'], name='Update Inventory')
+    def update_inventory(self, request, pk=None):
+        product = self.get_object()
+        serializer = ProductInventorySerializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=400)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
